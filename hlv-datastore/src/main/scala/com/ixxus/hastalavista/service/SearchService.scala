56cdd8d0828e2150c54408aa1ca03163ea925501
Alexandru Neatu <alexandru.neatu@ixxus.com>
2017-05-17 17:53:28 +0100

    [NOJIRA] - First attempt to implement the distance

--- hlv-datastore/src/main/scala/com/ixxus/hastalavista/service/SearchService.scala
+++ hlv-datastore/src/main/scala/com/ixxus/hastalavista/service/SearchService.scala
@@ -17,14 +17,34 @@ class SearchService extends AbstractService {
         if (terms.size == 1) {
             anaStore.addWord(term)
             val found = pageStore.pages
-                //.filter(p => p.parsedContents.getOrElse(term, 0) != 0)
                 .filter(p => p.delimContents.contains(term))
+                .toList
+                .sortWith(_.parsedContents.getOrElse(term, 0) > _.parsedContents.getOrElse(term, 0))
+
             result = (for (f <- found) yield f.puid + " " + f.url + " --- " + term + " / " + f.parsedContents.get(term)
                 + " / no searches: " + anaStore.words.getOrElse(term, 0)).mkString("\n")
         } else {
+            def diff(list: List[Int]): Int = if (list.isEmpty || list.tail.isEmpty) 0 else list.head - list.tail.head + diff(list.tail)
+
             anaStore.addTerm(term)
             terms.map(t => anaStore.addWord(t))
-            result = ""
+            val found = pageStore.pages
+                    .map(p => (p.puid,
+                                p.url,
+                                terms.map(kw => p.parsedContents.getOrElse(kw, 0)).toList.sum,
+                                -diff(terms.map(kw => p.delimContents.indexOf(kw)).toList)))
+                    .filter(_._3 > 0)
+                    .filter(t => t._4 > 0 && t._4 < 100)
+                    .toList
+                    .sortBy(t => (-t._3, t._4))
+                    //.sortBy(_._4)
+                    //.sortWith((x, y) => (x._4 > y._4 && x._3 < y._3) )
+                    //.sortBy(t => (t._3,t._4))
+                    //.sortWith(_._3 > _._3)
+                    //.sortWith(_._4 < _._4)
+                //.filter(p => p.delimContents.zipWithIndex.find((terms(0), _)) == p.delimContents.zipWithIndex.find((terms(0),_)) + 1)
+            result = (for (f <- found) yield f._1 + " " + f._2 + " --- " + term + " / " + f._3
+                + " / distance: " + f._4).mkString("\n")
         }
 
         result
--- hlv-datastore/src/main/scala/com/ixxus/hastalavista/store/AnaStore.scala
+++ hlv-datastore/src/main/scala/com/ixxus/hastalavista/store/AnaStore.scala
@@ -3,7 +3,8 @@ package com.ixxus.hastalavista.store
 import scala.collection.immutable.HashSet
 
 /**
-  * Analytics related Store
+  * Analytics related Store.
+  * Updates the words and terms used in search queries.
   *
   * Created by aneatu
   */
@@ -13,9 +14,14 @@ trait AnaStoreComponent {
         var words: Map[String, Int]
         var terms: Map[String, Int]
 
-        def addWord(word: String) { words updated (word, words.getOrElse(word, 0) + 1) }
-        def addTerm(term: String) { terms updated (term, terms.getOrElse(term, 0) + 1) }
-        //def updateMap[String, Int](map: Map[String, Int], key: String) =  map updated (key, map.getOrElse(key, 0) + 1)
+        def addWord(word: String) = { words += word -> (words.getOrElse(word, 0) + 1) }
+        def addTerm(term: String) = { terms += term -> (terms.getOrElse(term, 0) + 1) }
+
+        //def updateMap[String, Int](map: Map[String, Int], key: String) =  map updated (key, map.getOrElse(key, 0) + 1)  -- this is wrong because it uses generics
+        //def updateMap(map: Map[String, Int], key: String): Map[String, Int] = map updated(key, map.getOrElse(key, 0) + 1) -- nice one
+
+        // This could be used but multiple maps fields are defined
+        //def +=(t: (String, Int)) {words +=t}
     }
 
     val anaStore: AnaStore
